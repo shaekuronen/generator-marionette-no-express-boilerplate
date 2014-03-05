6f672dfc86d3646457180b1aee6ed261a7c2cbd9
Shae Kuronen <shae.kuronen@gmail.com>
2014-03-05 06:06:42 -0800

    moved requirejs config to config.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,6 +112,8 @@ module.exports = function (grunt) {
 
                     preserveLicenseComments: true,
 
+                    mainConfigFile: './scripts/config.js',
+
                     // https://github.com/jrburke/r.js/blob/master/build/example.build.js
                     optimize: 'uglify',
                     uglify: {
@@ -131,58 +133,6 @@ module.exports = function (grunt) {
                         //Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
                         no_mangle: true
                     },
-
-                    /* starting point for application */
-                    deps: ['backbone.marionette', 'bootstrap', 'main'],
-
-
-                    shim: {
-                        backbone: {
-                            deps: [
-                                'underscore',
-                                'jquery'
-                            ],
-                            exports: 'Backbone'
-                        },
-                        bootstrap: {
-                            deps: ['jquery'],
-                            exports: 'jquery'
-                        }
-                    },
-
-                    paths: {
-                        'templates': '../.tmp/scripts/templates',
-                        jquery: '../bower_components/jquery/jquery',
-                        backbone: '../bower_components/backbone-amd/backbone',
-                        underscore: '../bower_components/underscore-amd/underscore',
-
-                        /* alias all marionette libs */
-                        'backbone.marionette': '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
-                        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
-                        'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
-
-                        /* alias the bootstrap js lib */
-                        bootstrap: 'vendor/bootstrap',
-
-                        /* Alias text.js for template loading and shortcut the templates dir to tmpl */
-                        text: '../bower_components/requirejs-text/text',
-                        tmpl: "../templates",
-
-                        /* handlebars from the require handlerbars plugin below */
-                        handlebars: '../bower_components/require-handlebars-plugin/Handlebars',
-
-                        /* require handlebars plugin - Alex Sexton */
-                        i18nprecompile: '../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
-                        json2: '../bower_components/require-handlebars-plugin/hbs/json2',
-                        hbs: '../bower_components/require-handlebars-plugin/hbs',
-
-                        // collections
-                        collection_demo_linked: 'collections/collection_demo_linked'
-                    },
-
-                    hbs: {
-                        disableI18n: true
-                    }
 
                 }
             }
